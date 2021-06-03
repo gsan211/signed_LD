a22aad533eb2576bd85b6d584e06bd3fba092c93
gsan211 <36670832+gsan211@users.noreply.github.com>
2021-06-03 14:00:15 -0400

    Update prepare_vcf_for_R_drosophila.sh

--- drosophila_scripts/prepare_vcf_for_R_drosophila.sh
+++ drosophila_scripts/prepare_vcf_for_R_drosophila.sh
@@ -1,7 +1,7 @@
 bcftoolsg=/plas1/george.sandler/apps/bcftools-1.10.2/bin/bcftools
 
 #pull sites of interest from drosophila VCF and write to .txt file as a table
-#then input table intro "fix_dgrp_gt_script.sh" script to fix the genotypes
+#then input table intro "drosophila_edit_genotypes.sh" script to fix the genotypes
 #then LD can be calculated
 
 $bcftoolsg query -f "%ALT\t%CHROM\t%POS[\t%GT]\n" "../ZI_allchrom_clean_bi_samples.ann.vcf" -T ../ZI_utr_max5.list > ../ZI_all_UTR_GTS.txt
